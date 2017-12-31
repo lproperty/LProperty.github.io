c68f9ab97e7c4a021a0b92f38fb903039b8deb8a
Liu Peng <liu_peng@u.nus.edu>
2017-12-31 13:31:18 +0800

    delay change

--- static/js/pre-loader.js
+++ static/js/pre-loader.js
@@ -1,5 +1,5 @@
 $(window).load(function() {
   // Animate loader off screen
   //Adjusted delay time for development
-  $(".se-pre-con").delay(10000).fadeOut("slow");
+  $(".se-pre-con").delay(8000).fadeOut("slow");
 });
